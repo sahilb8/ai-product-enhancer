fdbffebd78368e90b51ae1617e41cc44db210663
sahil.b <sahil.b@aavishk-tech.com>
2025-08-16 19:38:38 +0530

    generate_description body fixes

--- python-ai-service/app/routers/products.py
+++ python-ai-service/app/routers/products.py
@@ -4,23 +4,28 @@ from app.schemas.product import ProductRequest
 
 router = APIRouter(prefix="/products", tags=["products"])
 
-LLM_API_URL = "https://api.openai.com/v1/chat/completions"
-API_KEY = 'your_api_key_here'  # Replace with your actual API key
+LLM_API_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent"
+API_KEY = ''  # Replace with your actual API key
 
 @router.post("/generate-description")
 async def generate_description(request: ProductRequest):
     headers = {
-        "Authorization": "Bearer {API_KEY}",
+        "X-goog-api-key": API_KEY,
         "Content-Type": "application/json"
     }
     payload = {
-        "model": "gpt-4o-mini",
-        "messages": [
-            {"role": "user", "content": "Generate a marketing description for {request.product_name}."}
+        "contents": [
+        {
+            "parts": [
+            {
+                "text": "Generate a marketing description for {request.productName}."
+            }
+            ]
+        }
         ]
     }
     async with httpx.AsyncClient() as client:
         response = await client.post(LLM_API_URL, json=payload, headers=headers)
         response.raise_for_status() # Raise an exception for bad status codes
         llm_response = response.json()
-        return {"description": llm_response["choices"]["message"]["content"]}
+        return {"description": llm_response["candidates"][0]["content"]}
